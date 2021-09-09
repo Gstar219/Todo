b6d12b55026725435e1da6e881bc36f0935c009f
Makinde gbolahan <44204442+Gstar219@users.noreply.github.com>
2021-09-09 18:10:41 +0100

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+
+  public task:any=[]
+
+  create(title:any){
+  
+    if (title!==''){
+    this.task.push({title})
+      console.log(this.task)
+localStorage.setItem('id',JSON.stringify(this.task))
+
+ }
+}
+get(){
+  return localStorage.getItem(JSON.parse(this.task))
+}
+delete(i:any){
+this.task.splice(i,1)
+}
 }
