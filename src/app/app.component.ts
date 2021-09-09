42e4729a98c80a6625692ece72cb9ada839a3a17
Makinde gbolahan <44204442+Gstar219@users.noreply.github.com>
2021-09-09 18:24:17 +0100

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,21 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  public task:any=[]
-
-  create(title:any){
-  
-    if (title!==''){
-    this.task.push({title})
-      console.log(this.task)
-localStorage.setItem('id',JSON.stringify(this.task))
-
- }
-}
-get(){
-  return localStorage.getItem(JSON.parse(this.task))
-}
-delete(i:any){
-this.task.splice(i,1)
-}
 }
