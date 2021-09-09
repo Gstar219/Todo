323eaa006aa093df123a1683a27c0766a9d093f1
Makinde gbolahan <44204442+Gstar219@users.noreply.github.com>
2021-09-09 18:24:30 +0100

    master

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -8,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class TodoComponent implements OnInit {
 
   constructor() { }
+  public task:any=[]
 
+  create(title:any){
+  
+    if (title!==''){
+    this.task.push({title})
+      console.log(this.task)
+localStorage.setItem('id',JSON.stringify(this.task))
+
+ }
+}
+get(){
+  return localStorage.getItem(JSON.parse(this.task))
+}
+delete(i:any){
+this.task.splice(i,1)
+}
   ngOnInit(): void {
   }
 
